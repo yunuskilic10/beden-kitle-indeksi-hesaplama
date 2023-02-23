c3d74d397f616eb7d66de5851abf99822fe0c4f6
yunuskilic10 <114607516+yunuskilic10@users.noreply.github.com>
2023-02-23 19:44:56 +0300

    Add files via upload
    vücut kitle indeksi tıp literatüründeki değerleri kullanarak vücudumuzun tipini belirleyerek yazdığımız program

---
+++ proje/vücut kitle indeksi hesaplama/vücut kitle indeksi hesaplama/Program.cs
@@ -0,0 +1,56 @@
+﻿double boy;
+double ağırlık;
+double vki;
+Console.WriteLine("boyu metre cinsinden girin");
+bool cevap1 = double.TryParse(Console.ReadLine(), out boy);
+Console.WriteLine("ağırlığı kg cinsinden girin");
+bool cevap2 = double.TryParse(Console.ReadLine(), out ağırlık);
+
+while (cevap1 == false || cevap2 == false)
+
+{
+    Console.WriteLine("sayı girin");
+    cevap1 = double.TryParse(Console.ReadLine(), out boy);
+    cevap2 = double.TryParse(Console.ReadLine(), out ağırlık);
+    continue;
+}
+while (boy>=3)
+{
+    Console.WriteLine("metre cinsinden giriniz");
+    cevap1 = double.TryParse(Console.ReadLine(), out boy);
+
+    continue;
+}
+do
+{
+
+    vki = ağırlık / (boy * boy);
+
+    if (vki>18 && vki<25)
+
+    {
+        Console.WriteLine("vücut kitle indeksi normaldir");
+        break;
+    }
+    else if (vki>25&& vki<30)
+    {
+        Console.WriteLine("vücut kitle indeksi kilolu");
+        break;
+    }
+    else if (vki>30)
+    {
+        Console.WriteLine("vücut kitle indeksi obez");
+        break;
+    }
+    else if (vki>35)
+    {
+        Console.WriteLine("vücut kitle indeksi ciddi obez");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("vücut kitle indeksi tıp tanımlamalarına girmez");
+        break;
+    }
+} while (boy>0&&boy<3);
+
